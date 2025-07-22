cefcbd953761fcf0040f2ee6ebf0cedffca2d309
ChaitanyaMardane <chaitanyamardane2003@gmail.com>
2025-07-22 15:44:19 +0530

    add domain route

--- app.js
+++ app.js
@@ -92,9 +92,9 @@ app.use("/",user);
 
 
 
-app.all("*",(req,res,next)=>{
-    next(new ExpressError(404,'Page Not Found'))
-})
+// app.all("*",(req,res,next)=>{
+//     next(new ExpressError(404,'Page Not Found'))
+// })
 app.use((err,req,res,next)=>{
     let {statuscode=500,message="Something Went Wrong"}=err;
     console.log(err.stack);
